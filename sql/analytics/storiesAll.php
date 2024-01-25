be01ccbf4f874b74c759976e58a09c6b18c13074
Andrimihamina <andrymihamina7@gmail.com>
2024-01-25 15:12:18 +0300

    modif

--- sql/analytics/storiesAll.php
+++ sql/analytics/storiesAll.php
@@ -2,7 +2,7 @@
     $sql_out= "SELECT flux_out.id_flux_out, flux_out.created_date, flux_out.id_out, flux_out.services, flux_out.descriptions, flux_out.quantity_out, flux_out.orderer, flux_out.name_resp_storage, services.services_descriptions 
                 FROM flux_out 
                 INNER JOIN services ON services.id_service = flux_out.services 
-                WHERE done_resp_storage = 1";
+                WHERE done_resp_storage = 1 ORDER BY created_date DESC";
     $outs = $db->query($sql_out);
     $outs= $outs->fetchAll();
 ?>
--- sql/analytics/storiesPurchaseAll.php
+++ sql/analytics/storiesPurchaseAll.php
@@ -3,7 +3,7 @@
                     flux_in.descriptions, flux_in.quantity_added, flux_in.purchase_price, flux_in.name_adding 
                 FROM flux_in 
                 INNER JOIN suppliers ON suppliers.id_suppliers = flux_in.suppliers 
-                WHERE done = 1";
+                WHERE done = 1 ORDER BY created_date ASC";
     $ins = $db->query($sql_in);
     $ins= $ins->fetchAll();
 ?>
