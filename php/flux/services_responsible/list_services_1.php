bcbb68561e84f01da7578c251e8776fa633a934c
Andrimihamina <andrymihamina7@gmail.com>
2024-02-20 11:36:10 +0300

    alpabet

--- php/flux/services_responsible/list_services_1.php
+++ php/flux/services_responsible/list_services_1.php
@@ -3,5 +3,5 @@ require_once("../../env/dns_warehouse.php");
     
 $id_resp = $_SESSION["user"]["responsibility"];          
       
-$list_services= $mysqli -> query("SELECT id_service, services FROM service_responsibility WHERE `$id_resp` = 1");
+$list_services= $mysqli -> query("SELECT id_service, services FROM service_responsibility WHERE `$id_resp` = 1 ORDER BY services ASC");
 ?> 
